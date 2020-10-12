33ce2604a3bfa07032d28f4c32ce932fe2a93e40
UandersonRFS <uandersonrfs@gmail.com>
2020-10-12 11:54:35 -0300

    Teste de colisão no grid pela direita

--- cypress/integration/game/rules.spec.js
+++ cypress/integration/game/rules.spec.js
@@ -53,4 +53,46 @@ context('Login', () => {
       expect(startCol).to.equal(0);
     });
   });
+
+  it('should stay in the grid by right', () => {
+    for (let i = 0; i < 50; i++) {
+      cy.get('body').type('{rightarrow}');
+    }
+
+    cy.get('.grid > .grid-item').then(($gridItems) => {
+      let startRow = -1;
+      let endRow = -1;
+      let startCol = -1;
+      let endCol = -1;
+
+      const grid = [];
+
+      for (let i = 0; i < 16; i++) {
+        grid[i] = [];
+        for (let j = 0; j < 10; j++) {
+          if ($gridItems[i * 10 + j].classList.contains("block")) {
+            grid[i][j] = 1;
+  
+            if (startRow === -1) {
+              startRow = i;
+            }
+  
+            endRow = i;
+  
+            if (j < startCol || startCol === -1) {
+              startCol = j;
+            }
+  
+            if (j > endCol || endCol === -1) {
+              endCol = j;
+            }
+          } else {
+            grid[i][j] = 0;
+          }
+        }
+      }
+
+      expect(endCol).to.equal(9);
+    });
+  });
 });
