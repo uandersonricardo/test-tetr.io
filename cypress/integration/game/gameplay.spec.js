d65f99ed07f6982a62caad5ef302de91b1404727
UandersonRFS <uandersonrfs@gmail.com>
2020-10-12 11:41:05 -0300

    Teste de movimentação para esquerda

--- cypress/integration/game/gameplay.spec.js
+++ cypress/integration/game/gameplay.spec.js
@@ -6,15 +6,13 @@ context('Login', () => {
     cy.get('.swal2-input').type("Jogador");
     cy.get('.swal2-confirm').click();
     cy.wait(200);
-  });
-
-  it('should rotate tetromino', () => {
     for (let i = 0; i < 5; i++) {
       cy.get('body').type('{downarrow}');
     }
-
     cy.wait(200);
-    
+  });
+
+  it('should rotate tetromino', () => {
     let t1 = [];
     
     cy.get('.grid > .grid-item').then(($gridItems) => {
@@ -96,6 +94,84 @@ context('Login', () => {
       }
 
       expect(t2).to.deep.equal(t1);
+    });
+  });
+
+  it('should move tetromino left', () => {
+    let x = -1;
+    
+    cy.get('.grid > .grid-item').then(($gridItems) => {
+      let startRow = -1;
+      let endRow = -1;
+      let startCol = -1;
+      let endCol = -1;
+
+      const grid = [];
+
+      for (let i = 0; i < 16; i++) {
+        grid[i] = [];
+        for (let j = 0; j < 10; j++) {
+          if ($gridItems[i * 10 + j].classList.contains("block")) {
+            grid[i][j] = 1;
+  
+            if (startRow === -1) {
+              startRow = i;
+            }
+  
+            endRow = i;
+  
+            if (j < startCol || startCol === -1) {
+              startCol = j;
+            }
+  
+            if (j > endCol || endCol === -1) {
+              endCol = j;
+            }
+          } else {
+            grid[i][j] = 0;
+          }
+        }
+      }
+  
+      x = startCol;
+    });
+
+    cy.get('body').type('{leftarrow}');
+
+    cy.get('.grid > .grid-item').then(($gridItems) => {
+      let startRow = -1;
+      let endRow = -1;
+      let startCol = -1;
+      let endCol = -1;
+
+      const grid = [];
+
+      for (let i = 0; i < 16; i++) {
+        grid[i] = [];
+        for (let j = 0; j < 10; j++) {
+          if ($gridItems[i * 10 + j].classList.contains("block")) {
+            grid[i][j] = 1;
+  
+            if (startRow === -1) {
+              startRow = i;
+            }
+  
+            endRow = i;
+  
+            if (j < startCol || startCol === -1) {
+              startCol = j;
+            }
+  
+            if (j > endCol || endCol === -1) {
+              endCol = j;
+            }
+          } else {
+            grid[i][j] = 0;
+          }
+        }
+      }
+
+      expect(startCol).to.equal(x - 1);
     });
   });
 });
