5ab07e4a14ebd9920a8b3a4491b16acb4fbb20c1
UandersonRFS <uandersonrfs@gmail.com>
2020-10-12 11:44:36 -0300

    Teste de movimentação para baixo

--- cypress/integration/game/gameplay.spec.js
+++ cypress/integration/game/gameplay.spec.js
@@ -252,4 +252,82 @@ context('Login', () => {
       expect(startCol).to.equal(x + 1);
     });
   });
+
+  it('should move tetromino down', () => {
+    let y = -1;
+    
+    cy.get('.grid > .grid-item').then(($gridItems) => {
+      let startRow = -1;
+      let endRow = -1;
+      let startCol = -1;
+      let endCol = -1;
+
+      const grid = [];
+
+      for (let i = 0; i < 16; i++) {
+        grid[i] = [];
+        for (let j = 0; j < 10; j++) {
+          if ($gridItems[i * 10 + j].classList.contains("block")) {
+            grid[i][j] = 1;
+  
+            if (startRow === -1) {
+              startRow = i;
+            }
+  
+            endRow = i;
+  
+            if (j < startCol || startCol === -1) {
+              startCol = j;
+            }
+  
+            if (j > endCol || endCol === -1) {
+              endCol = j;
+            }
+          } else {
+            grid[i][j] = 0;
+          }
+        }
+      }
+  
+      y = startRow;
+    });
+
+    cy.get('body').type('{downarrow}');
+
+    cy.get('.grid > .grid-item').then(($gridItems) => {
+      let startRow = -1;
+      let endRow = -1;
+      let startCol = -1;
+      let endCol = -1;
+
+      const grid = [];
+
+      for (let i = 0; i < 16; i++) {
+        grid[i] = [];
+        for (let j = 0; j < 10; j++) {
+          if ($gridItems[i * 10 + j].classList.contains("block")) {
+            grid[i][j] = 1;
+  
+            if (startRow === -1) {
+              startRow = i;
+            }
+  
+            endRow = i;
+  
+            if (j < startCol || startCol === -1) {
+              startCol = j;
+            }
+  
+            if (j > endCol || endCol === -1) {
+              endCol = j;
+            }
+          } else {
+            grid[i][j] = 0;
+          }
+        }
+      }
+
+      expect(startRow).to.equal(y + 1);
+    });
+  });
 });
